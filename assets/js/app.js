6c6c22c0f2da47cf7ef3a5ac728fd7814960129f
Amrit <amritgagra200@gmail.com>
2023-12-05 12:33:05 +0530

    fixed some issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,29 @@ if(banner) {
   }
 }
 
+const formBtn = document.querySelector('.btn__form');
+const body = document.querySelector('body');
+const modal = document.querySelector('.modal');
+const mobileMenu = document.querySelector('.navbar-toggler');
+const closeBtn = document.querySelector('.btn-close');
+mobileMenu.addEventListener('click',function(){
+  header.classList.add('active')
+})
+closeBtn.addEventListener('click',function(){
+  header.classList.remove('active')
+})
+formBtn.addEventListener("click",function(){
+  if(body.classList.contains('modal-open')) {
+    body.classList.add('overflow-auto')
+    body.classList.add('p-0')
+  }
+  else {
+    body.classList.remove('overflow-auto')
+    body.classList.remove('p-0')
+  }
+})
+
+
 gsap.registerPlugin(ScrollTrigger);
 gsap.set('.resource__text span', { width: 0 })
 
@@ -30,74 +53,3 @@ gsap.timeline({
 
 
 
-
-// let faces = document.querySelector('.faces')
-// if(faces) {
-//   gsap.registerPlugin(ScrollTrigger)
-  
-//   let mm = gsap.matchMedia(),
-//   breakPoint = 992;
-
-// mm.add({
-// isDesktop: `(min-width: ${breakPoint}px)`,
-// isMobile: `(max-width: ${breakPoint - 1}px)`,
-// reduceMotion: "(prefers-reduced-motion: reduce)"
-
-// }, (context) => {
-// let { isDesktop, isMobile, reduceMotion } = context.conditions;
-
-// let tl = gsap.timeline({
-//   scrollTrigger: {
-//       trigger:'.faces',
-//       start:isDesktop ? 'top top' : '50% center' ,
-//       end:isDesktop ? '100% end' : '160% end',
-//       scrub:true,
-//       ease: 'linear',
-//       pin:true,
-//   }
-// });
-// gsap.set('.scroll-text',{
-// x:isDesktop ? "70vw" : "50vw",
-// // rotate:isDesktop ? 340 : 340,
-// })
-// tl.to('.scroll-text', {
-// duration:2,
-// x:isDesktop ? "-150vw" : "-150vw",
-// })
-// })
-//   }
-
-//   let faces1 = document.querySelector('.faces1')
-// if(faces1) {
-//   gsap.registerPlugin(ScrollTrigger)
-  
-//   let mm = gsap.matchMedia(),
-//   breakPoint = 576;
-
-// mm.add({
-// isDesktop: `(min-width: ${breakPoint}px)`,
-// isMobile: `(max-width: ${breakPoint - 1}px)`,
-// reduceMotion: "(prefers-reduced-motion: reduce)"
-
-// }, (context) => {
-// let { isDesktop, isMobile, reduceMotion } = context.conditions;
-
-// let tl = gsap.timeline({
-//   scrollTrigger: {
-//       trigger:'.faces1',
-//       start:isDesktop ? 'top center' : '50% center' ,
-//       end:isDesktop ? '100% end' : '100% end',
-//       scrub:true,
-//       ease: 'linear',
-//       pin:true,
-//   }
-// });
-// gsap.set('.scroll-text1',{
-// x:isDesktop ? "70vw" : "50vw",
-// })
-// tl.to('.scroll-text1', {
-// duration:2,
-// x:isDesktop ? "-150vw" : "-213vw",
-// })
-// })
-//   }
